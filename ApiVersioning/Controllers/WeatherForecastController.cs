61f503c77e68b49dde11e7f8ebf70cdb0cefba9d
Hitesh Pant <hitesh.pant@zeiss.com>
2020-05-29 18:36:58 +0530

    Add project files.

---
+++ ApiVersioning/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiVersioning.Controllers
+{
+    [ApiController]
+    //2 way of specifying version in url , but this m3ans client has to change the url to consume new vewrsion of api
+    //[Route("api/v{version:apiVersion}/[controller]")]
+    [Route("[controller]")]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] SummariesV1 = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [MapToApiVersion("1.0")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = SummariesV1[rng.Next(SummariesV1.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [MapToApiVersion("2.0")]
+        public IActionResult GetV3()
+        {
+            return new OkObjectResult("Response from versin 2");
+        }
+    }   
+}
---
+++ ApiVersioning/readme.txt
@@ -0,0 +1,6 @@
+﻿--------------Api Version can be done by 3 ways in .Net core
+---------package required : Microsoft.AspNetCore.Mvc.Versioning--------------
+1- Url Versioning{worst approach as all client needs to adapt to it}
+2- QueryString adding the version{still ok , but fragile by default hits default version}
+3- request Header sending the version number (good approach)
+ 
